006a3c836c6b80c093f5918f38d173d29829831c
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-09-03 23:12:53 +0200

    label and input from shadcn

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,16 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const AddTalent = () => {
-  return <div>AddTalent</div>;
+  return (
+    <div>
+      <form action="">
+        <Label htmlFor="terms">Name</Label>
+        <Input type="text" placeholder="name" />
+        <Label htmlFor="terms">Name</Label>
+      </form>
+    </div>
+  );
 };
 
 export default AddTalent;
