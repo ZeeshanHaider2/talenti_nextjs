19f02ae2af6552e682eaf43b1e519f523e9ef5e0
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-09-04 22:34:29 +0200

    talent form added

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,24 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 const AddTalent = () => {
   return (
-    <div>
-      <form action="">
+    <div className="w-full mt-8">
+      <form action="" className="flex flex-col gap-2">
         <Label htmlFor="terms">Name</Label>
         <Input type="text" placeholder="name" />
-        <Label htmlFor="terms">Name</Label>
+        <Label htmlFor="email">Email</Label>
+        <Input type="text" placeholder="email" />
+        <Label htmlFor="terms">Title</Label>
+        <Input type="text" placeholder="title" />
+        <Label htmlFor="terms">Skills</Label>
+        <textarea
+          type="text"
+          name="skills"
+          className="w-full font-normal outline outline-0 focus:outline-0 transition-all border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
+        ></textarea>
+        <Button>Submit</Button>
       </form>
     </div>
   );
